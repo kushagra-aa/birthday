69b5caa079bc0c433b2388e2f7f0d27ecc0dbac5
kushagra-aa <kushagraagnihotri24@gmail.com>
2021-06-18 03:46:23 +0530

    added name form

--- js/script.js
+++ js/script.js
@@ -1,62 +1,47 @@
-const wait = 3000;
-const text = ['Happy Birthday Khushi!', 'Stay Blessed!!', 'Mazzey Krro!!!', 'Partyyyyyyyyyy!!!!!!!'];
-const Typer = function (txtElement, wait, text) {
-    this.txtElement = txtElement;
-    this.text = text;
-    this.txt = '';
-    this.txtIndex = 0;
-    this.wait = wait;
-    this.type();
-    this.isdel = false;
+// html for wishing page
+const wish = `<div class="con">
+<div class="container text-center">
+<img src="./assets/Cake.svg" alt="Gift" />
+<h1>
+<span class="txt-type"></span>
+</h1>
+</div>
+<button class="new" onclick="neww()">new?</button>
+</div>`
+
+const neww = () => {
+    localStorage.clear();
+    location.reload();
 }
 
-Typer.prototype.type = function () {
-    // current index of word
-    const index = this.txtIndex % this.text.length;
-    // Get Full Text of current word
-    const fullTxt = this.text[index];
-    // check deleteing
-    if (this.isdel) {
-        // -character
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
+// page div from html
+let page = document.getElementById('page');
+var name, wishing;
+
+let submitHandler = (e) => {
+
+    if (
+        !document.getElementById('name').value
+    ) {
+        alert("Enter Name!!")
     }
     else {
-        // +character
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
+
+        e.preventDefault();
+        let name = document.getElementById('name').value;
+        page.innerHTML = wish;
+        wishing = true;
+        localStorage.setItem("name", name);
+        localStorage.setItem("wishing", wishing);
     }
-
-    // add text in html element
-    this.txtElement.innerHTML = `<span class="text">${this.txt}</span>`;
-
-    // initial type speed
-    let typeSpeed = 200;
-
-    if (this.isdel) {
-        typeSpeed /= 2;
-    }
-
-    // if word is complete
-    if (!this.isdel && this.txt === fullTxt) {
-        // make pause
-        typeSpeed = this.wait;
-        // set isdel true
-        this.isdel = true;
-    }
-    else if (this.isdel && this.txt === '') {
-        this.isdel = false;
-        // move to next wordd
-        this.txtIndex++;
-        // pause before next word
-        typeSpeed = 400;
-    }
-
-    // call function type every 500ms
-    setTimeout(() => this.type(), typeSpeed)
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('wishing')) {
+        page.innerHTML = wish;
+    }
+    else {
 
-function init() {
-    const txtElement = document.querySelector('.txt-type');
-    new Typer(txtElement, wait, text);
-}
+        document.getElementById('button').addEventListener('click', submitHandler);
+    }
+});
---
+++ js/wish.js
@@ -0,0 +1,71 @@
+// import { wishing, name } from "./script";
+
+// wait time for typer
+const wait = 3000;
+// the text that will be displayed
+const text = [`Happy Birthday ${localStorage.getItem('name')}!`, 'Stay Blessed!!', 'Mazzey Krro!!!', 'Partyyyyyyyyyy!!!!!!!'];
+
+// typer function
+const Typer = function (txtElement, wait, text) {
+    this.txtElement = txtElement;
+    this.text = text;
+    this.txt = '';
+    this.txtIndex = 0;
+    this.wait = wait;
+    this.type();
+    this.isdel = false;
+}
+
+Typer.prototype.type = function () {
+    // current index of word
+    const index = this.txtIndex % this.text.length;
+    // Get Full Text of current word
+    const fullTxt = this.text[index];
+    // check deleteing
+    if (this.isdel) {
+        // -character
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    }
+    else {
+        // +character
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // add text in html element
+    this.txtElement.innerHTML = `<span class="text">${this.txt}</span>`;
+
+    // initial type speed
+    let typeSpeed = 200;
+
+    if (this.isdel) {
+        typeSpeed /= 2;
+    }
+
+    // if word is complete
+    if (!this.isdel && this.txt === fullTxt) {
+        // make pause
+        typeSpeed = this.wait;
+        // set isdel true
+        this.isdel = true;
+    }
+    else if (this.isdel && this.txt === '') {
+        this.isdel = false;
+        // move to next wordd
+        this.txtIndex++;
+        // pause before next word
+        typeSpeed = 400;
+    }
+
+    // call function type every 500ms
+    setTimeout(() => this.type(), typeSpeed)
+}
+
+
+const init = () => {
+    if (localStorage.getItem('wishing')) {
+        const txtElement = document.querySelector('.txt-type');
+        new Typer(txtElement, wait, text);
+    }
+}
+
+window.addEventListener('load', init);
